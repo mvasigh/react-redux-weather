102d0051c341838b9125b6efc92946ee60539ead
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-05 07:01:39 -0500

    display map in place of city name

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import LineChart from '../components/line_chart';
 import { connect } from 'react-redux';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
-  renderWeather(cityData, i) {
+  renderWeather(cityData) {
     const data = {};
     const keys = ['temp', 'pressure', 'humidity'];
     keys.forEach(key => {
       data[key] = cityData.list.map(entry => entry.main[key]);
     });
+    const { lat, lon } = cityData.city.coord;
 
     return (
-      <tr key={i}>
-        <td>{cityData.city.name}</td>
+      <tr key={cityData.city.name}>
+        <td>
+          <GoogleMap lat={lat} lon={lon} />
+        </td>
         {Object.values(data).map((entry, i) => (
           <td key={i}>
             <LineChart data={entry} limit={10} width={120} height={80} />
