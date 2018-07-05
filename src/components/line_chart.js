f55b4307a10d32e281024d0e7fc5a3b916b59fcb
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-05 07:01:24 -0500

    explicitly name LineChart component function

--- src/components/line_chart.js
+++ src/components/line_chart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
-export default props => {
+const LineChart = props => {
   return (
     <Sparklines {...props}>
       <SparklinesLine color="blue" />
     </Sparklines>
   );
 };
+
+export default LineChart;
